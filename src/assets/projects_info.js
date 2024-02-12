3c8ebc1144db768751031f82352dd75b39ecb24d
Macbook <sucevanalex06@gmail.com>
2024-02-12 13:54:36 +0100

    Bricks info changes

--- src/assets/projects_info.js
+++ src/assets/projects_info.js
@@ -1,6 +1,6 @@
 export const info = {
   bricks:
-    "Visualization of rectangle packaging algorithm which I wrote by myself, every window resize it recalculate the positions, every rectangle has its random color, and if rectangles are the same width and height the color is also the same.",
+    "Visualization of rectangle packaging algorithm which I wrote by myself. The 'Rectangular Block Packing Algorithm' arranges a collection of rectangular blocks within a canvas to achieve optimal space utilization, avoiding overlaps through strategic placement and rotation. Every window resize it recalculate the positions, every rectangle has its random color, and if rectangles are the same width and height the color is also the same.",
   tictactoe: "This project features a Tic Tac Toe game enhanced with an AI opponent powered by the minimax algorithm. The AI calculates its moves to ensure an optimal strategy, making it a challenging opponent even for skilled players. With the minimax algorithm in action, the game guarantees a draw if both players play without mistakes, showcasing the AI's strategic prowess.",
   };
 

ec994e2c7161d7430bbd77922b27dd6072942a14
TheAlexGET <sucevanalex06@gmail.com>
2024-03-02 12:49:05 +0100

    Added Github Link

--- src/components/MyLinks.jsx
+++ src/components/MyLinks.jsx
@@ -3,6 +3,8 @@ import {useSelector} from 'react-redux'
 import gmail from '/src/assets/gmail.png'
 import linkedin from '/src/assets/linkedin.png'
 import telegram from '/src/assets/telegram.png'
+import githubBlack from '/src/assets/githubBlack.png'
+import githubWhite from '/src/assets/githubWhite.png'
 const MyLinks = () => {
   const theme = useSelector((state) => (state.theme))
   return (
@@ -12,6 +14,7 @@ const MyLinks = () => {
         <a target="_blank" href="mailto:sucevanalex06@gmail.com"><img src={gmail} alt="" className='links__img'/></a>
         <a target="_blank" href="https://www.linkedin.com/in/alexandr-sucevan-834216267"><img src={linkedin} alt="" className='links__img'/></a>
         <a target="_blank" href="https://t.me/sucevan"><img src={telegram} alt="" className='links__img'/></a>
+        <a href="https://github.com/TheAlexGET" target='_blank'><img src={theme == 'dark' ? githubWhite : githubBlack} alt="" className='links__img'/></a>
       </div>
       <div className={theme == 'dark' ? 'links__linkDark' : 'links__linkLight'}>sucevanalex06@gmail.com</div>
     </div>
